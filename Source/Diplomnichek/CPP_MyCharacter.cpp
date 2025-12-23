e6dfd8c05f45e1e7af04404656ddf386e9bbe91f
krytoy_dobrinya <mixailovuyriy@gmail.com>
2025-12-23 19:19:26 +0300

    Механика света

--- Source/Diplomnichek/CPP_MyCharacter.cpp
+++ Source/Diplomnichek/CPP_MyCharacter.cpp
@@ -5,11 +5,65 @@
 
 ACPP_MyCharacter::ACPP_MyCharacter()
 {
-    PrimaryActorTick.bCanEverTick = false;
+    PrimaryActorTick.bCanEverTick = true;
 }
 
-void ACPP_MyCharacter::CPP_Move_func(FVector2D Direction)
+void ACPP_MyCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // Проверяем источники света каждый кадр
+    if (lighting_sources_counter <= 0)
+    {
+        // Если нет источников света и мы еще не умираем
+        if (!bDeathTimerActive)
+        {
+            CPP_StartDeathTimer();
+            bDeathTimerActive = true;
+        }
+    }
+    else
+    {
+        // Если есть источники света и таймер смерти активен
+        if (bDeathTimerActive)
+        {
+            CPP_StopDeathTimer();
+            bDeathTimerActive = false;
+        }
+    }
+}
+
+void ACPP_MyCharacter::CPP_MoveFunc(FVector2D Direction)
 {
     // Движение по осям X и Y
     AddMovementInput(FVector(Direction.X, Direction.Y, 0.0f));
+}
+
+void ACPP_MyCharacter::CPP_StartDeathTimer()
+{
+    if (!bDeathTimerActive)
+    {
+        bDeathTimerActive = true;
+        GetWorld()->GetTimerManager().SetTimer(DeathTimerHandle, this, &ACPP_MyCharacter::CPP_DeathTimerComplete, DeathDelay, false);
+        GEngine->AddOnScreenDebugMessage(-1, 4.0f, FColor::White, TEXT("Таймер запущен"));
+    }
+    
+}
+
+void ACPP_MyCharacter::CPP_StopDeathTimer()
+{
+    if (bDeathTimerActive)
+    {
+        bDeathTimerActive = false;
+        GetWorld()->GetTimerManager().ClearTimer(DeathTimerHandle);
+        GEngine->AddOnScreenDebugMessage(-1, 4.0f, FColor::White, TEXT("Таймер остановлен"));
+    }
+
+}
+
+void ACPP_MyCharacter::CPP_DeathTimerComplete()
+{
+    bDeathTimerActive = false;
+        GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("ВЫ УМЕРЛИ"), true, FVector2D(10.0f, 10.0f)
+    );
 }
--- Source/Diplomnichek/CPP_MyCharacter.h
+++ Source/Diplomnichek/CPP_MyCharacter.h
@@ -11,10 +11,28 @@ class DIPLOMNICHEK_API ACPP_MyCharacter : public ACharacter
 {
     GENERATED_BODY()
 
+private:
+    bool Invulnerability = false;     // Неуязвимость темноте (Если есть в руках источник света)
+    bool bDeathTimerActive = false;   // Флаг таймера смерти
+    float DeathDelay = 2.0f;          // Cчетчик до смерти (В секундах)
+    FTimerHandle DeathTimerHandle;    // Таймер смерти
+
 public:
     ACPP_MyCharacter();
-    
-    
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lighting")
+    int lighting_sources_counter = 0; // Количество источников света
+
     UFUNCTION(BlueprintCallable, Category = "Movement")
-    void CPP_Move_func(FVector2D Direction);
+    void CPP_MoveFunc(FVector2D Direction);
+
+    UFUNCTION(BlueprintCallable, Category = "Lighting")
+    void CPP_StartDeathTimer();
+
+    UFUNCTION(BlueprintCallable, Category = "Lighting")
+    void CPP_StopDeathTimer();
+
+    void CPP_DeathTimerComplete();
+
+    virtual void Tick(float DeltaTime) override;
 };
