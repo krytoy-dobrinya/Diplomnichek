adac47de91a6ec144db37b33059c846d8f50ad2f
Артем <karljonson22@gmail.com>
2026-05-12 19:22:50 +0300

    Improved building system
    имеются ошибки при работе с инвентарем

--- Source/Diplomnichek/BPC_Inventory_Component.cpp
+++
@@ -1,6 +0,0 @@
-#include "BPC_Inventory_Component.h"
-
-UBPC_Inventory_Component::UBPC_Inventory_Component()
-{
-    PrimaryComponentTick.bCanEverTick = false;
-}
--- Source/Diplomnichek/ConstructionManager.h
+++ Source/Diplomnichek/ConstructionManager.h
@@ -25,9 +25,6 @@ public:
     UFUNCTION(BlueprintCallable, Category = "Building")
     void CancelPlacement();
 
-    UFUNCTION(BlueprintCallable, Category = "Building")
-    bool HasRequiredItems(UBuildingData* Data) const;
-
     // Данные текущей постройки
     UPROPERTY(BlueprintReadOnly, Category = "Building")
     UBuildingData* CurrentBuildingData;
